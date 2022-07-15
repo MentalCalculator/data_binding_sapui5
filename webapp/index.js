054ed31a12d71ab574580e310effb9ee14e12dcd
Deniss Ozerskis <deniss.ozerskis@ecenta.com>
2022-07-15 13:32:27 +0000

    Step 6: Resource Models

--- webapp/index.js
+++ webapp/index.js
@@ -2,24 +2,31 @@ sap.ui.require([
 	"sap/m/Text",
 	"sap/ui/model/json/JSONModel",
     "sap/ui/core/mvc/XMLView",
-    "sap/ui/model/BindingMode"
-], function (Text, JSONModel, XMLView, BindingMode) {
+    "sap/ui/model/resource/ResourceModel"
+], function (Text, JSONModel, XMLView, ResourceModel) {
 	"use strict";
 
 	// Attach an anonymous function to the SAPUI5 'init' event
 	sap.ui.getCore().attachInit(function () {
 		// Create a JSON model from an object literal
-		var oModel = new JSONModel({
+		let oModel = new JSONModel({
 			firstName: "Humana",
             lastName: "Calculator",
             enabled: true,
             panelHeaderText: "Data Binding Basics"
 		});
 
-        oModel.setDefaultBindingMode(BindingMode.OneWay);
-
 		// Assign the model object to the SAPUI5 core
 		sap.ui.getCore().setModel(oModel);
+
+        let oResourceModel = new ResourceModel({
+			bundleName: "sap.ui.demo.db.i18n.i18n",
+			supportedLocales: ["", "lv"],
+			fallbackLocale: ""
+		});
+
+        // Assign the model object to the SAPUI5 core using the name "i18n"
+		sap.ui.getCore().setModel(oResourceModel, "i18n");
 
 		// Display the XML view called "App"
 		new XMLView({
